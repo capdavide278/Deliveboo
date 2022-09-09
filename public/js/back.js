848903d894ac67fe0bc925d9e957c178fd4653ac
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-09-09 16:04:42 +0200

    prova

--- public/js/back.js
+++ public/js/back.js
@@ -27915,7 +27915,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Davide\Desktop\Deliveboo\resources\js\back.js */"./resources/js/back.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\php_esercizi\laravel\Deliveboo\resources\js\back.js */"./resources/js/back.js");
 
 
 /***/ })
