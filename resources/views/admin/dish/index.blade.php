9b86354222aebad1843b8385d33f2405f8fcf327
Gloria <gloghe@yahoo.it>
2022-09-13 14:22:32 +0200

    spazi inesistenti

--- resources/views/admin/dish/index.blade.php
+++ resources/views/admin/dish/index.blade.php
@@ -12,7 +12,7 @@
                 phone
             </th>
         </thead>
-        <tbody> 
+        <tbody>
                 @foreach ($dishes as $dish)
                     <tr>
                         <td>
@@ -29,20 +29,20 @@
                         </td>
                         <td class="actions">
                             <a href="{{ route('admin.dish.show', ['dish' => $dish]) }}" class="btn btn-primary">View</a>
-                            
+
                             @if(Auth::id() == $dish->restaurant_id)
                                 <a href="{{ route('admin.dish.edit', ['dish' => $dish]) }}" class="btn btn-warning">Edit</a>
                                 {{-- DESTROY FORM --}}
                                 <form action="{{ route('admin.dish.destroy', ['dish' => $dish]) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
-        
+
                                     <button class="btn btn-danger"  type="submit">Delete</button>
                                 </form>
                             @endif
                         </td>
-                    </tr>    
-                @endforeach        
+                    </tr>
+                @endforeach
         </tbody>
     </table>
 @endsection
